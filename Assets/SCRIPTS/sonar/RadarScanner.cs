8ed6a27d67605b76540ccafa89c3b3275a81c5d2
frknocak <furkan.ocak@std.yildiz.edu.tr>
2025-05-02 16:52:24 +0300

    objects are shown as dots on the minimap

---
+++ Assets/SCRIPTS/sonar/RadarScanner.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class RadarScanner : MonoBehaviour
+{
+    public float scanRange = 10f;
+    public int numberOfRays = 60;
+    public float rotationSpeed = 30f;
+
+    public RadarUI radarUI; // RadarUI scriptine baðlan
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+        float angleStep = 360f / numberOfRays;
+
+        for (int i = 0; i < numberOfRays; i++)
+        {
+            float angle = i * angleStep;
+            Vector3 dir = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+            Ray ray = new Ray(transform.position, dir);
+            if (Physics.Raycast(ray, out RaycastHit hit, scanRange))
+            {
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.green);
+                    radarUI.ShowPing(hit.point); // Noktayý göster
+                }
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, dir * scanRange, Color.red);
+            }
+        }
+    }
+}
